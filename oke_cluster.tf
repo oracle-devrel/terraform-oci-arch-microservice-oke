b9eb695dcad9727cfc0c3a94bfa6a0b3313f68d3
Eli Schilling <schilling.eli@gmail.com>
2024-01-17 12:25:57 -0800

    variable updates
    corrected node_pool_shape variable and increased OKE cluster version to latest.

--- oke_cluster.tf
+++ oke_cluster.tf
@@ -9,7 +9,7 @@ module "oci-oke" {
   oke_cluster_name                  = var.cluster_name
   cluster_type                      = var.cluster_type
   ssh_public_key                    = tls_private_key.public_private_key_pair.public_key_openssh
-  node_shape                        = var.node_shape
+  node_shape                        = var.node_pool_shape
   node_ocpus                        = var.node_pool_flex_shape_ocpus
   node_memory                       = var.node_pool_flex_shape_memory
   node_count                        = var.node_pool_size
